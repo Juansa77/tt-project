594acaa08b0a3ab02de583ea0bf8f02ea8615093
Juansa77 <dedalus1977@gmail.com>
2023-08-02 20:06:54 +0200

    added component charge in horizontal scrolling

--- src/assets/src/components/GameSelectionComponent/GameSelectionComponent.jsx
+++ src/assets/src/components/GameSelectionComponent/GameSelectionComponent.jsx
@@ -1,28 +1,31 @@
-import { FaPlus, FaMinus, FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import "./GameSelectionComponent.css";
+import { Navigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useGameContext } from "../../contexts/GameContext";
 
-const GameSelectionComponent = ({
-  searchFunction,
-  searchTerm,
-}) => {
+
+const GameSelectionComponent = ({ category, searchFunction, searchTerm }) => {
   const [response, setResponse] = useState([]);
   const [scrollPosition, setScrollPosition] = useState(0);
-  const[containerWidth, setContainerWidth] = useState(0)
+  const { setSelectedGame } = useGameContext();
+  const navigate = useNavigate();
 
+
+  //*-----LÓGICA PARA EL SCROLL HORIZONTAL-----
   const handleScroll = (direction) => {
     console.log("entra");
     const container = document.getElementById("gameSelectionContainer");
-    const containerWidth = (container.offsetWidth/1.8)
-    const containerScrollWidth = (container.scrollWidth/1.8);
+    const containerWidth = container.offsetWidth / 1.2;
+    const containerScrollWidth = container.scrollWidth / 1.2;
     console.log(containerWidth);
-    console.log(containerScrollWidth)
-  
+    console.log(containerScrollWidth);
+
     if (direction === "left") {
       setScrollPosition(Math.max(0, scrollPosition - 600));
-     
     } else if (direction === "right") {
-        console.log(scrollPosition)
+      console.log(scrollPosition);
       const maxScrollRight = containerScrollWidth - containerWidth;
       const newScrollPosition = Math.min(maxScrollRight, scrollPosition + 600);
       if (newScrollPosition !== scrollPosition) {
@@ -31,6 +34,7 @@ const GameSelectionComponent = ({
     }
   };
 
+  //* ---LÓGICA PARA EL FETCH----
   const search = searchTerm;
   useEffect(() => {
     const fetchData = async () => {
@@ -38,22 +42,28 @@ const GameSelectionComponent = ({
       setResponse(data);
     };
     fetchData();
-    
   }, [search, searchFunction]);
 
 
-  useEffect(() => {
-    // Recalculate the container width when response changes
-    const updateContainerWidth = () => {
-      const responseLength = response?.data?.length || 0;
-      setContainerWidth(20 * responseLength);
-    };
-    updateContainerWidth();
-  }, [response]);
 
-  console.log(containerWidth)
+  console.log(scrollPosition);
+
+//** -----LÓGICA PARA NAVIGATE Y LLEVAR A DETAIL
+
+  const handleSelectGame = (game) => {
+    console.log(game)
+    setSelectedGame(game);
+    // Redirige a la página de detalles del juego seleccionado
+    navigate(`/games/${game._id}`);
+
+  };
+
+
   return (
     <div>
+    <div className="CategoryTextContainer">
+      <h2 className="CategoryText ">{category}</h2>
+    </div>
       <div
         id="gameSelectionContainer"
         className="game-selection-container"
@@ -61,15 +71,11 @@ const GameSelectionComponent = ({
           overflowX: "hidden",
           transition: "transform 0.3s ease", // Agrega la transición para el desplazamiento suave
           transform: `translateX(-${scrollPosition}px)`,
-       
         }}
       >
         {response?.data?.map((game, index) => (
-          <div
-            className="card-container-selection"
-            key={index}
-          >
-          
+          <div className="card-container-selection" key={index}>
+          <div onClick={() => handleSelectGame(game)}>
             <img
               className="image-selection"
               src={game.image}
@@ -78,15 +84,26 @@ const GameSelectionComponent = ({
             <div className="title-container-selection">
               <h2 className="title-selection">{game.title}</h2>
             </div>
+            </div>
           </div>
         ))}
       </div>
 
-      <div className="scroll-buttons-container" style={{
-         maxWidth: `100vw`
-        }}>
-        <button onClick={() => handleScroll("left")}><FaChevronLeft/></button>
-        <button onClick={() => handleScroll("right")}><FaChevronRight/></button>
+      <div
+        className="scroll-buttons-container"
+        style={{
+          maxWidth: `100vw`,
+        }}
+      >
+        <button
+          className="arrow-btn"
+          onClick={() => handleScroll("left")}
+        >
+          <FaChevronLeft   style={scrollPosition == 0 ? { fill: 'transparent' } : {}} />
+        </button>
+        <button className="arrow-btn" onClick={() => handleScroll("right")}>
+          <FaChevronRight />
+        </button>
       </div>
     </div>
   );
--- src/assets/src/pages/HomePage/Home.jsx
+++ src/assets/src/pages/HomePage/Home.jsx
@@ -1,52 +1,53 @@
 import "./Home.css";
 import HeroCard from "../../components/HeroCard";
-import CategorySplitter from "../../components/CategorySplitter";
-import GameCard from "../../components/GameCard";
-import GameSelection from "../../components/GameSelection";
-GameSelectionComponent
+GameSelectionComponent;
 import { useEffect, useState } from "react";
 import { gameByPlayingTime } from "../../services/API_USER/game.service";
 import GameSelectionComponent from "../../components/GameSelectionComponent/GameSelectionComponent";
 
-
-
-
 const Home = () => {
+  const [verticalScrollPosition, setVerticalScrollPosition] = useState(0);
 
-  const [response, setResponse] = useState()
+  //* ---LÓGICA PARA CALCULAR EL DESPLAZAMIENTO HORIZONTAL----
+  useEffect(() => {
+    const handleScroll = () => {
+      setVerticalScrollPosition(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-  useEffect(()=>{
-    const playingTime = "20%20Min"
-    const fetchData = async()=>{
-    const data =  await gameByPlayingTime(playingTime)
-    setResponse(data)}
-    fetchData()
-  },[])
+  console.log(verticalScrollPosition);
 
-  const gamebyTime = gameByPlayingTime
-  const searchTerm = "20%20Min"
-
-  console.log(response)
   return (
-    <div className="home">
-    <div className="hero">
-      <HeroCard
-        image="ceramic1.jpg"
-        title="Busca gente para jugar en tu ciudad"
-        description="Descripción del juego"
-      />
-       <CategorySplitter title="Categoría" />
+    <div id="homeDiv" className="home">
+      <div className="hero">
+        <HeroCard
+          image="ceramic1.jpg"
+          title="Busca gente para jugar en tu ciudad"
+          description="Descripción del juego"
+        />
       </div>
-     
-<div className="gameContainer">
-
-
-<GameSelectionComponent
-        searchFunction={gameByPlayingTime}
-        searchTerm={searchTerm}
-      />
-</div>
-
+      {verticalScrollPosition >= 0 && (
+        <div className={`gameContainer ${verticalScrollPosition >= 0 ? "visible" : ""}`}>
+          <GameSelectionComponent
+            category={"Para partidas rápidas"}
+            searchFunction={gameByPlayingTime}
+            searchTerm={"20 Min"}
+          />
+        </div>
+      )}
+      {verticalScrollPosition >= 10 && (
+        <div className={`gameContainer ${verticalScrollPosition >= 30 ? "visible" : ""}`}>
+          <GameSelectionComponent
+            category={"Si tienes una horilla..."}
+            searchFunction={gameByPlayingTime}
+            searchTerm={"60 Min"}
+          />
+        </div>
+      )}
     </div>
   );
 };
